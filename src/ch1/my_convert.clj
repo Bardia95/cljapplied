757e8bfc15fb74c3fee14f7e28c651b4fd47c3fb
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-20 22:19:44 -0400

    Added convert code examples

--- src/ch1/my_convert.clj
+++ src/ch1/my_convert.clj
@@ -5,4 +5,19 @@
   (fn [unit1 unit2 quantity] unit1 unit2))
 
 ;; lb to oz
+(defmethod convert [:lb :oz] [_ _ lb] (* lb 16))
+
+;; oz to lb
+(defmethod convert [:oz :lb] [_ _ oz] (/ oz 16))
+
+;; fallthrough
+(defmethod convert :default [u1 u2 q]
+  (if (= u1 u2)
+    q
+    (assert false (str "Unknown unit conversion from " u1 " to " u2))))
+
+(defn ingredient+
+  "Add two ingredients into   single ingredient, combining their quantities with unit conversion if necessary"
+  [{q1 :quantity u1 :unit :as i1} {q2 :quantity u2 :unit}]
+  (assoc i1 :quantity (+ q1 (convert u2 u1 q2))))
 
